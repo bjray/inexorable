f35452a5054a8f0203abaabf06020aba2760c5a5
bjray <bjandraney@mac.com>
2015-07-06 22:44:02 -0500

    Initial Commit

---
+++ Inexorable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Inexorable
+//
+//  Created by B.J. Ray on 7/6/15.
+//  Copyright (c) 2015 Sitting Circles, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Inexorable/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Inexorable
+//
+//  Created by B.J. Ray on 7/6/15.
+//  Copyright (c) 2015 Sitting Circles, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
